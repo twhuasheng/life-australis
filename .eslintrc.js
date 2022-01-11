3dd2b8c922507137b3e2fdcbe1f4bc94d8c215b3
twhuasheng <ting.wang11@outlook.com>
2022-01-11 14:18:53 +1100

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,25 @@ module.exports = {
   },
   plugins: ['react', 'jest'],
   rules: {
+    'jsx-a11y/label-has-associated-control': 'off',
     'object-curly-newline': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': 'off',
     'react/function-component-definition': 'off',
     'react/jsx-one-expression-per-line': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/no-default-export': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'internal', 'external'],
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
